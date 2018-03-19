ced887b473dbb07b766789a4aec5e7a9953d58e6
Alexqzhou <dsotsen@npurobot.com>
2018-03-19 19:52:08 +0800

    add

---
+++ SIMD/ASM_SSE.cpp
@@ -0,0 +1,324 @@
+#include "stdafx.h"
+#include <xmmintrin.h>
+
+void VSFastMulASM(const Matrix3x3W & InM1, const Matrix3x3W & InM2, Matrix3x3W & OutM)
+{
+    __asm
+    {
+        mov		eax, [InM2]
+        mov		ecx, [InM1]
+        movups	xmm4, [eax]
+        movups	xmm5, [eax + 16]
+        movups	xmm6, [eax + 32]
+        movups	xmm7, [eax + 48] // _mm_loadu_ps 
+
+        // 1
+        movss	xmm0, [ecx] //_mm_load_ss
+        shufps	xmm0, xmm0, 0 //_mm_shuffle_ps 
+        mulps	xmm0, xmm4
+
+        movss	xmm1, [ecx + 4]
+        shufps	xmm1, xmm1, 0
+        mulps	xmm1, xmm5
+
+        movss	xmm2, [ecx + 8]
+        shufps	xmm2, xmm2, 0
+        mulps	xmm2, xmm6
+
+        addps	xmm1, xmm0			// Row 1
+
+        movss	xmm3, [ecx + 12]
+        shufps	xmm3, xmm3, 0
+        mulps	xmm3, xmm7
+
+        // 2
+        movss	xmm0, [ecx + 16]
+        shufps	xmm0, xmm0, 0
+        mulps	xmm0, xmm4
+
+        addps	xmm3, xmm2			// Row 1
+
+        movss	xmm2, [ecx + 20]
+        shufps	xmm2, xmm2, 0
+        mulps	xmm2, xmm5
+
+        addps	xmm3, xmm1			// Row 1
+
+        mov		eax, [OutM]
+
+        movss	xmm1, [ecx + 24]
+        shufps	xmm1, xmm1, 0
+        mulps	xmm1, xmm6
+
+        movups[eax], xmm3			// Row 1 out
+
+        addps	xmm2, xmm0			// Row 2
+
+        movss	xmm3, [ecx + 28]
+        shufps	xmm3, xmm3, 0
+        mulps	xmm3, xmm7
+
+        // 3
+        movss	xmm0, [ecx + 32]
+        shufps	xmm0, xmm0, 0
+        mulps	xmm0, xmm4
+
+        addps	xmm3, xmm1			// Row 2
+
+        movss	xmm1, [ecx + 36]
+        shufps	xmm1, xmm1, 0
+        mulps	xmm1, xmm5
+
+        addps	xmm3, xmm2			// Row 2
+
+        movss	xmm2, [ecx + 40]
+        shufps	xmm2, xmm2, 0
+        mulps	xmm2, xmm6
+
+        movups[eax + 16], xmm3		// Row 2 out
+
+        addps	xmm1, xmm0			// Row 3
+
+        movss	xmm3, [ecx + 44]
+        shufps	xmm3, xmm3, 0
+        mulps	xmm3, xmm7
+
+        // 4
+        movss	xmm0, [ecx + 48]
+        shufps	xmm0, xmm0, 0
+        mulps	xmm0, xmm4
+
+        addps	xmm3, xmm2			// Row 3
+
+        movss	xmm2, [ecx + 52]
+        shufps	xmm2, xmm2, 0
+        mulps	xmm2, xmm5
+
+        addps	xmm3, xmm1			// Row 3
+
+        movss	xmm1, [ecx + 56]
+        shufps	xmm1, xmm1, 0
+        mulps	xmm1, xmm6
+
+        movups[eax + 32], xmm3		// Row 3 out
+
+        addps	xmm2, xmm0			// Row 4
+
+        movss	xmm3, [ecx + 60]
+        shufps	xmm3, xmm3, 0
+        mulps	xmm3, xmm7
+
+        addps	xmm3, xmm1			// Row 4
+
+        addps	xmm3, xmm2			// Row 4
+
+        movups[eax + 48], xmm3		// Row 4 out
+    }
+}
+
+void VSFastCrossASM(const Vector3 &InV1, const Vector3 &InV2, Vector3 &OutV)
+{
+    __asm
+    {
+        mov ecx, [InV1];
+        mov eax, [InV2];
+
+        movups xmm0, [ecx];
+        movups xmm1, [eax];
+
+        movaps xmm2, xmm0;
+        movaps xmm3, xmm1;
+
+        shufps xmm0, xmm0, _MM_SHUFFLE(0, 0, 2, 1);
+        shufps xmm1, xmm1, _MM_SHUFFLE(0, 1, 0, 2);
+
+        mulps xmm0, xmm1;
+
+        mov eax, [OutV];
+
+        shufps xmm2, xmm2, _MM_SHUFFLE(0, 1, 0, 2);
+        shufps xmm3, xmm3, _MM_SHUFFLE(0, 0, 2, 1);
+
+        mulps xmm2, xmm3;
+        subps xmm0, xmm2;
+
+        movss[eax], xmm0;
+        shufps xmm0, xmm0, _MM_SHUFFLE(3, 2, 1, 1);
+        movss[eax + 4], xmm0;
+        shufps xmm0, xmm0, _MM_SHUFFLE(3, 2, 1, 2);
+        movss[eax + 8], xmm0;
+    }
+}
+
+void VSFastNormalizeASM(const Vector2 &InV, Vector2 &OutV)
+{
+    const float fThree = 3.0f;
+    const float fOneHalf = 0.5f;
+    __asm
+    {
+        mov ecx, [InV];
+        movups xmm0, [ecx];
+        movaps xmm5, xmm0;
+        mulps xmm0, xmm0;
+
+        movaps xmm1, xmm0;
+        shufps xmm0, xmm0, _MM_SHUFFLE(0, 0, 0, 0);
+        shufps xmm1, xmm1, _MM_SHUFFLE(1, 1, 1, 1);
+
+        addss xmm1, xmm0;
+        rsqrtss xmm0, xmm1;
+        // Newton-Raphson iteration
+        movss xmm3, [fThree];
+        movss xmm2, xmm0;
+        mulss xmm0, xmm1;
+        mulss xmm0, xmm2;
+        mulss xmm2, [fOneHalf];
+        subss xmm3, xmm0;
+        mulss xmm3, xmm2;
+
+        xorps xmm4, xmm4;
+        cmpss xmm4, xmm1, 4;
+
+        andps xmm3, xmm4;
+
+        shufps xmm3, xmm3, 0;
+        mulps xmm3, xmm5;
+        mov ecx, [OutV];
+
+        movss[ecx], xmm3;
+
+        shufps xmm3, xmm3, _MM_SHUFFLE(1, 1, 1, 1);
+        movss[ecx + 4], xmm3;
+    }
+}
+
+void VSFastNormalizeASM(const Vector3 &InV, Vector3 &OutV)
+{
+    const float fThree = 3.0f;
+    const float fOneHalf = 0.5f;
+    __asm
+    {
+        mov ecx, [InV];
+        movups xmm0, [ecx];
+        movaps xmm5, xmm0;
+        mulps xmm0, xmm0;
+
+        movaps xmm1, xmm0;
+        movaps xmm2, xmm0;
+        shufps xmm1, xmm1, _MM_SHUFFLE(1, 1, 1, 1);
+        shufps xmm2, xmm2, _MM_SHUFFLE(2, 2, 2, 2);
+
+        addss xmm1, xmm0;
+        addss xmm1, xmm2;
+        rsqrtss xmm0, xmm1;
+        // Newton-Raphson iteration
+        movss xmm3, [fThree];
+        movss xmm2, xmm0;
+        mulss xmm0, xmm1;
+        mulss xmm0, xmm2;
+        mulss xmm2, [fOneHalf];
+        subss xmm3, xmm0;
+        mulss xmm3, xmm2;
+
+        xorps xmm4, xmm4;
+        cmpss xmm4, xmm1, 4;
+
+        andps xmm3, xmm4;
+
+        shufps xmm3, xmm3, 0;
+        mulps xmm3, xmm5;
+        mov ecx, [OutV];
+
+        movss[ecx], xmm3;
+
+        shufps xmm3, xmm3, _MM_SHUFFLE(3, 2, 1, 1);
+        movss[ecx + 4], xmm3;
+
+        shufps xmm3, xmm3, _MM_SHUFFLE(3, 2, 1, 2);
+        movss[ecx + 8], xmm3;
+    }
+}
+
+void VSFastNormalizeASM(const Vector3W &InV, Vector3W &OutV)
+{
+    const float fThree = 3.0f;
+    const float fOneHalf = 0.5f;
+    __asm
+    {
+        mov ecx, [InV];
+        movups xmm0, [ecx];
+        movaps xmm5, xmm0;
+        mulps xmm0, xmm0;
+
+        movaps xmm1, xmm0;
+
+        shufps xmm0, xmm0, _MM_SHUFFLE(0, 0, 2, 0);
+        shufps xmm1, xmm1, _MM_SHUFFLE(0, 0, 3, 1);
+
+        addps xmm1, xmm0;
+
+        movaps xmm2, xmm1;
+        shufps xmm2, xmm2, _MM_SHUFFLE(1, 1, 1, 1);
+
+        addss xmm1, xmm2;
+
+        rsqrtss xmm0, xmm1;
+        // Newton-Raphson iteration
+        movss xmm3, [fThree];
+        movss xmm2, xmm0;
+        mulss xmm0, xmm1;
+        mulss xmm0, xmm2;
+        mulss xmm2, [fOneHalf];
+        subss xmm3, xmm0;
+        mulss xmm3, xmm2;
+
+        xorps xmm4, xmm4;
+        cmpss xmm4, xmm1, 4;
+
+        andps xmm3, xmm4;
+
+        shufps xmm3, xmm3, 0;
+        mulps xmm3, xmm5;
+        mov ecx, [OutV];
+
+        movups[ecx], xmm3;
+    }
+}
+
+float VSFastLengthASM(const Vector3 &vec)
+{
+    float rt = 0.f;
+    const float fThree = 3.0f;
+    const float fOneHalf = 0.5f;
+    __asm
+    {
+        mov ecx, [vec];
+        movups xmm0, [ecx];
+        mulps xmm0, xmm0;
+
+        movaps xmm1, xmm0;
+        movaps xmm2, xmm0;
+        shufps xmm1, xmm1, _MM_SHUFFLE(1, 1, 1, 1);
+        shufps xmm2, xmm2, _MM_SHUFFLE(2, 2, 2, 2);
+
+        addss xmm1, xmm0;
+        addss xmm1, xmm2;
+        rsqrtss xmm0, xmm1;
+        // Newton-Raphson iteration
+        movss xmm3, [fThree];
+        movss xmm2, xmm0;
+        mulss xmm0, xmm1;
+        mulss xmm0, xmm2;
+        mulss xmm2, [fOneHalf];
+        subss xmm3, xmm0;
+        mulss xmm3, xmm2;
+
+        xorps xmm4, xmm4;
+        cmpss xmm4, xmm1, 4;
+
+        mulss xmm3, xmm1;
+        andps xmm3, xmm4;
+        movss[rt], xmm3;
+    }
+    return rt;
+}
---
+++ SIMD/SIMD.cpp
@@ -0,0 +1,149 @@
+#include "stdafx.h"
+#include <xmmintrin.h>
+
+void VSFastMulSIMD(const Matrix3x3W & InM1, const Matrix3x3W & InM2, Matrix3x3W & OutM)
+{
+    // unaligned memory ? row major ?
+    __m128 m11 = _mm_loadu_ps((const float*)&InM1);
+    __m128 m12 = _mm_loadu_ps((const float*)&InM1 + 4);
+    __m128 m13 = _mm_loadu_ps((const float*)&InM1 + 8);
+    __m128 m14 = _mm_loadu_ps((const float*)&InM1 + 12);
+
+    __m128 m21 = _mm_loadu_ps((const float*)&InM2);
+    __m128 m22 = _mm_loadu_ps((const float*)&InM2 + 4);
+    __m128 m23 = _mm_loadu_ps((const float*)&InM2 + 8);
+    __m128 m24 = _mm_loadu_ps((const float*)&InM2 + 12);
+
+    {
+        __m128 e0 = _mm_shuffle_ps(m21, m21, _MM_SHUFFLE(0, 0, 0, 0));
+        __m128 e1 = _mm_shuffle_ps(m21, m21, _MM_SHUFFLE(1, 1, 1, 1));
+        __m128 e2 = _mm_shuffle_ps(m21, m21, _MM_SHUFFLE(2, 2, 2, 2));
+        __m128 e3 = _mm_shuffle_ps(m21, m21, _MM_SHUFFLE(3, 3, 3, 3));
+
+        __m128 m0 = _mm_mul_ps(m11, e0);
+        __m128 m1 = _mm_mul_ps(m12, e1);
+        __m128 m2 = _mm_mul_ps(m13, e2);
+        __m128 m3 = _mm_mul_ps(m14, e3);
+
+        __m128 a0 = _mm_add_ps(m0, m1);
+        __m128 a1 = _mm_add_ps(m2, m3);
+        __m128 a2 = _mm_add_ps(a0, a1);
+
+        _mm_storeu_ps(OutM.M[0], a2);
+    }
+
+    {
+        __m128 e0 = _mm_shuffle_ps(m22, m22, _MM_SHUFFLE(0, 0, 0, 0));
+        __m128 e1 = _mm_shuffle_ps(m22, m22, _MM_SHUFFLE(1, 1, 1, 1));
+        __m128 e2 = _mm_shuffle_ps(m22, m22, _MM_SHUFFLE(2, 2, 2, 2));
+        __m128 e3 = _mm_shuffle_ps(m22, m22, _MM_SHUFFLE(3, 3, 3, 3));
+
+        __m128 m0 = _mm_mul_ps(m11, e0);
+        __m128 m1 = _mm_mul_ps(m12, e1);
+        __m128 m2 = _mm_mul_ps(m13, e2);
+        __m128 m3 = _mm_mul_ps(m14, e3);
+
+        __m128 a0 = _mm_add_ps(m0, m1);
+        __m128 a1 = _mm_add_ps(m2, m3);
+        __m128 a2 = _mm_add_ps(a0, a1);
+
+        _mm_storeu_ps(OutM.M[1], a2);
+    }
+
+    {
+        __m128 e0 = _mm_shuffle_ps(m23, m23, _MM_SHUFFLE(0, 0, 0, 0));
+        __m128 e1 = _mm_shuffle_ps(m23, m23, _MM_SHUFFLE(1, 1, 1, 1));
+        __m128 e2 = _mm_shuffle_ps(m23, m23, _MM_SHUFFLE(2, 2, 2, 2));
+        __m128 e3 = _mm_shuffle_ps(m23, m23, _MM_SHUFFLE(3, 3, 3, 3));
+
+        __m128 m0 = _mm_mul_ps(m11, e0);
+        __m128 m1 = _mm_mul_ps(m12, e1);
+        __m128 m2 = _mm_mul_ps(m13, e2);
+        __m128 m3 = _mm_mul_ps(m14, e3);
+
+        __m128 a0 = _mm_add_ps(m0, m1);
+        __m128 a1 = _mm_add_ps(m2, m3);
+        __m128 a2 = _mm_add_ps(a0, a1);
+
+        _mm_storeu_ps(OutM.M[2], a2);
+    }
+
+    {
+        __m128 e0 = _mm_shuffle_ps(m24, m24, _MM_SHUFFLE(0, 0, 0, 0));
+        __m128 e1 = _mm_shuffle_ps(m24, m24, _MM_SHUFFLE(1, 1, 1, 1));
+        __m128 e2 = _mm_shuffle_ps(m24, m24, _MM_SHUFFLE(2, 2, 2, 2));
+        __m128 e3 = _mm_shuffle_ps(m24, m24, _MM_SHUFFLE(3, 3, 3, 3));
+
+        __m128 m0 = _mm_mul_ps(m11, e0);
+        __m128 m1 = _mm_mul_ps(m12, e1);
+        __m128 m2 = _mm_mul_ps(m13, e2);
+        __m128 m3 = _mm_mul_ps(m14, e3);
+
+        __m128 a0 = _mm_add_ps(m0, m1);
+        __m128 a1 = _mm_add_ps(m2, m3);
+        __m128 a2 = _mm_add_ps(a0, a1);
+
+        _mm_storeu_ps(OutM.M[3], a2);
+    }
+}
+
+void VSFastCrossSIMD(const Vector3 &InV1, const Vector3 &InV2, Vector3 &OutV)
+{
+    __m128 v1 = _mm_setr_ps(InV1.x, InV1.y, InV1.z, 0);
+    __m128 v2 = _mm_setr_ps(InV2.x, InV2.y, InV2.z, 0);
+    __m128 c = _mm_sub_ps(
+        _mm_mul_ps(_mm_shuffle_ps(v1, v1, _MM_SHUFFLE(3, 0, 2, 1)), _mm_shuffle_ps(v2, v2, _MM_SHUFFLE(3, 1, 0, 2))),
+        _mm_mul_ps(_mm_shuffle_ps(v1, v1, _MM_SHUFFLE(3, 1, 0, 2)), _mm_shuffle_ps(v2, v2, _MM_SHUFFLE(3, 0, 2, 1)))
+    );
+    OutV.x = *(float*)(&c);
+    OutV.y = *((float*)(&c) + 1);
+    OutV.z = *((float*)(&c) + 2);
+}
+
+void VSFastNormalizeSIMD(const Vector2 &InV, Vector2 &OutV)
+{
+    const float fThree = 3.0f;
+    const float fOneHalf = 0.5f;
+    __m128 v1 = _mm_setr_ps(InV.x, InV.y, 0, 0);
+    __m128 v2 = _mm_mul_ps(v1, v1);
+    __m128 v3 = _mm_shuffle_ps(v2, v2, _MM_SHUFFLE(3, 2, 0, 1));
+    __m128 v4 = _mm_shuffle_ps(v3, v3, _MM_SHUFFLE(3, 2, 0, 1));
+    __m128 v5 = _mm_add_ps(v3, v4);
+    __m128 v6 = _mm_rsqrt_ps(v5);
+    __m128 v7 = _mm_mul_ps(v1, v6);
+    OutV.x = *(float*)&v7;
+    OutV.y = *((float*)&v7 + 1);
+}
+
+void VSFastNormalizeSIMD(const Vector3 &InV, Vector3 &OutV)
+{
+    const float fThree = 3.0f;
+    const float fOneHalf = 0.5f;
+    __m128 v1 = _mm_setr_ps(InV.x, InV.y, InV.z, 0);
+    __m128 v2 = _mm_mul_ps(v1, v1);
+    __m128 v3 = _mm_shuffle_ps(v2, v2, _MM_SHUFFLE(1, 1, 1, 1));
+    __m128 v4 = _mm_shuffle_ps(v2, v2, _MM_SHUFFLE(2, 2, 2, 2));
+    __m128 v5 = _mm_add_ps(_mm_add_ps(v3, v4), v2);
+    __m128 v6 = _mm_shuffle_ps(v5, v5, _MM_SHUFFLE(0, 0, 0, 0));
+    __m128 v7 = _mm_rsqrt_ps(v6);
+    __m128 v8 = _mm_mul_ps(v1, v7);
+    OutV.x = *(float*)&v8;
+    OutV.y = *((float*)&v8 + 1);
+    OutV.z = *((float*)&v8 + 2);
+}
+
+void VSFastNormalizeSIMD(const Vector3W &InV, Vector3W &OutV)
+{
+
+}
+
+float VSFastLengthSIMD(const Vector3 &vec)
+{
+    __m128 v1 = _mm_setr_ps(vec.x, vec.y, vec.z, 0);
+    __m128 v2 = _mm_mul_ps(v1, v1);
+    __m128 v3 = _mm_shuffle_ps(v2, v2, _MM_SHUFFLE(1, 1, 1, 1));
+    __m128 v4 = _mm_shuffle_ps(v2, v2, _MM_SHUFFLE(2, 2, 2, 2));
+    __m128 v5 = _mm_add_ps(_mm_add_ps(v3, v4), v2);
+    __m128 v7 = _mm_sqrt_ps(v5);
+    return *(float*)&v7;
+}
